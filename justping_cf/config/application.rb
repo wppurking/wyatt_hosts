040fdf3573d5c1089987609ede2b1e6ca99836ed
wyatt <wppurking@gmail.com>
2012-11-19 00:44:00 +0800

    * 判断 cloudfoundry 环境与本地环境的差异处理 mongoid 的配置信息 * 调整 js,css 文件路径

--- justping_cf/config/application.rb
+++ justping_cf/config/application.rb
@@ -4,8 +4,9 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
+# Uncomment this line for Rails 3.1+
+require "sprockets/railtie"
 #require "rails/test_unit/railtie"
-# require "sprockets/railtie" # Uncomment this line for Rails 3.1+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -37,8 +38,6 @@ module JustpingCf
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    config.serve_static_assets = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
--- justping_cf/config/initializers/01_mongoid.rb
+++ justping_cf/config/initializers/01_mongoid.rb
@@ -2,11 +2,9 @@ require "mongoid"
 # 根据 VCAP_SERVICES 的环境选择需要初始化的 mongodb id:port;
 # 如果是没有 VCAP_SERVICES 的本地环境, 则使用回 mongoid_.yml 文件
 Mongoid.configure do |config|
-  config.load!(Rails.root.join("config", "mongoid_.yml")) unless ENV['VCAP_SERVICES']
-  conn_info = nil
-
-
+  puts ENV['VCAP_SERVICES']
   if ENV['VCAP_SERVICES']
+    conn_info = nil
     services = JSON.parse(ENV['VCAP_SERVICES'])
     services.each do |service_version, bindings|
       bindings.each do |binding|
@@ -17,40 +15,39 @@ Mongoid.configure do |config|
       end
     end
     raise "could not find connection info for mongo" unless conn_info
+
+    options = {
+        identity_map_enabled: true,
+        allow_dynamic_fields: false,
+        include_root_in_json: false,
+        raise_not_found_error: false
+    }
+
+    ip = "#{conn_info['hostname']}:#{conn_info['port'].to_i}"
+
+    # debug info
+    puts "IP: #{ip}"
+    puts "DB: #{conn_info['db']}"
+    puts "#{conn_info.to_json}"
+
+    # TODO 为什么在这里拿到了正确 cloudfoundry 的 mongodb service 但无法初始化?
+    #{"hostname":"172.30.48.68","host":"172.30.48.68","port":25233,"username":"6da5650d-d11f-447d-a88d-73cc9feb9665","password":"1fa1b234-01b2-40ff-96db-5c5afa69cbf1","name":"33ab5652-e536-4d53-bd6a-a101a7c61372","db":"db","url":"mongodb://6da5650d-d11f-447d-a88d-73cc9feb9665:1fa1b234-01b2-40ff-96db-5c5afa69cbf1@172.30.48.68:25233/db"}}
+    puts ENV['VCAP_SERVICES']
+    sessions = {
+        default: {
+            database: conn_info['db'] || 'justping',
+            hosts: [ip],
+            url: conn_info['url']
+        }
+    }
+    if conn_info['username'] and conn_info['password']
+      sessions[:default][:username] = conn_info['username']
+      sessions[:default][:password] = conn_info['password']
+    end
+
+    config.options = options
+    config.sessions = sessions
   else
-    conn_info = {hostname: '127.0.0.1', port: 27017}
+    config.load!(Rails.root.join("config", "mongoid_.yml").to_s)
   end
-
-  options = {
-      identity_map_enabled: true,
-      allow_dynamic_fields: false,
-      include_root_in_json: false,
-      raise_not_found_error: false
-  }
-
-  ip = "#{conn_info['hostname']}:#{conn_info['port'].to_i}"
-
-  # debug info
-  puts "IP: #{ip}"
-  puts "DB: #{conn_info['db']}"
-  puts "#{conn_info.to_json}"
-
-  # TODO 为什么在这里拿到了正确 cloudfoundry 的 mongodb service 但无法初始化?
-  #{"hostname":"172.30.48.68","host":"172.30.48.68","port":25233,"username":"6da5650d-d11f-447d-a88d-73cc9feb9665","password":"1fa1b234-01b2-40ff-96db-5c5afa69cbf1","name":"33ab5652-e536-4d53-bd6a-a101a7c61372","db":"db","url":"mongodb://6da5650d-d11f-447d-a88d-73cc9feb9665:1fa1b234-01b2-40ff-96db-5c5afa69cbf1@172.30.48.68:25233/db"}}
-  puts ENV['VCAP_SERVICES']
-  sessions = {
-      default: {
-          database: conn_info['db'] || 'justping',
-          hosts: [ip],
-          url: conn_info['url']
-      }
-  }
-  if conn_info['username'] and conn_info['password']
-    sessions[:default][:username] = conn_info['username']
-    sessions[:default][:password] = conn_info['password']
-  end
-
-
-  config.options = options
-  config.sessions = sessions
 end
