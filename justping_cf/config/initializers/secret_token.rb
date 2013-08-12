147d35bc5ab2fba6e98942b03d0d6c7211a44d0b
wyatt <wppurking@gmail.com>
2013-08-12 11:16:53 +0800

    remove cloudfoundry version

--- justping_cf/config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# -*- encoding : utf-8 -*-
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-JustpingCf::Application.config.secret_token = 'e456f4dbeddef3e6132fab363da9b464b96314fd816babb1207d06f8372d3d39e24819a1825ee6f5acb18c84a35f0d6f73698409c11372f4d52ae8e2dff791b8'
